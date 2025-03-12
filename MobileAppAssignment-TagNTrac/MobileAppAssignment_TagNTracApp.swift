31d6305ef1e96b047c036e8d9527a3c4a79cafa1
Muhammad Midlaj <muhammad@surveyheart.com>
2025-03-12 12:51:41 +0530

    Initial Commit

---
+++ MobileAppAssignment-TagNTrac/MobileAppAssignment_TagNTracApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppAssignment_TagNTracApp.swift
+//  MobileAppAssignment-TagNTrac
+//
+//  Created by Muhammad on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppAssignment_TagNTracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
